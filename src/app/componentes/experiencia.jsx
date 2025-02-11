4a006da0d9e998aafef71d7ef86c308311109ec0
santiskap <santiagotula1@gmail.com>
2025-02-11 10:55:47 -0300

    Revert "Modificaciones experiencia"
    This reverts commit c75a411de1c23eb33901a765d9c8632e78e4364f.

--- src/app/componentes/experiencia.jsx
+++ src/app/componentes/experiencia.jsx
@@ -13,13 +13,8 @@ export function Experiencia() {
                     <div>
                         <div className="pb-2">
                             <h3 className="font-semibold pt-6 text-gray-800 text-lg">2016 - al presente</h3>
-<<<<<<< HEAD
                             <h3 className="font-semibold text-2xl rosa">UI/UX Designer & React Frontend developer</h3>
                             <p> <strong>Freelace</strong> Diseño de interfaces, experiencia de usuario, desarrollo html/css, programación javascript. React Js. Wordpress</p>
-=======
-                            <h3 className="font-semibold text-2xl rosa">Sr UI/UX Designer & React Frontend developer</h3>
-                            <p> <strong>Freelace</strong> Figma designer. Frontend Developer, UX - UI Designer y Wordpress developer. Desarrollo html/css/javascript</p>
->>>>>>> 2747b3a (Modificaciones experiencia)
                         </div>
                     </div>
                 </div>
@@ -27,40 +22,17 @@ export function Experiencia() {
                     <div>
                         <div className="pb-2">
                             <h3 className="font-semibold pt-6 text-gray-800 text-lg">2020-2023</h3>
-<<<<<<< HEAD
                             <h3 className="font-semibold text-2xl rosa">Frontend Developer - UI/UX Designer</h3>
-=======
-                            <h3 className="font-semibold text-2xl rosa">Sr Frontend Developer - Sr UI/UX Designer</h3>
->>>>>>> 2747b3a (Modificaciones experiencia)
                             <p> <strong>FLACSO</strong> Frontend Developer, UX/UI Designer & Wordpress developer. Product Manager. Scrum master.</p>
                         </div>
                     </div>
                 </div>
 
-<<<<<<< HEAD
                 <div className="flex flex-row"> 
                     <div>
                         <div className="pb-2">
                             <h3 className="font-semibold pt-6 text-gray-800 text-lg">2016</h3>
                             <h3 className="font-semibold text-2xl rosa">UI Designer - Frontend developer</h3>
-=======
-                <div className="flex flex-row">
-                    <div>
-                        <div className="pb-2">
-                            <h3 className="font-semibold pt-6 text-gray-800 text-lg">2017-2019</h3>
-                            <h3 className="font-semibold text-2xl rosa">Sr Front-end Developer</h3>
-                            <p> <strong>Latinlingua</strong> Frontend developer, Pixel Perfect, CSS3, HTML5, Javascript y Jquery.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="flex flex-row"> 
-                    <div>
-                        <div className="pb-2">
-                            <h3 className="font-semibold pt-6 text-gray-800 text-lg">2016 - 2017</h3>
-                            <h3 className="font-semibold text-2xl rosa">UI Designer - Sr Frontend developer</h3>
->>>>>>> 2747b3a (Modificaciones experiencia)
                             <p> <strong>Agencia Selnet</strong>. Diseño y programación de interfaces para diversos sitios y aplicaciones web desarrollados en HTML5, CSS3, Bootstrap y Wordpress.
                             </p>
                         </div>
@@ -70,13 +42,8 @@ export function Experiencia() {
                 <div className="flex flex-row">
                     <div>
                         <div className="pb-2">
-<<<<<<< HEAD
                             <h3 className="font-semibold pt-6 text-gray-800 text-lg">2014-2015</h3>
                             <h3 className="font-semibold text-2xl rosa">Front-end Developer</h3>
-=======
-                            <h3 className="font-semibold pt-6 text-gray-800 text-lg">2013-2015</h3>
-                            <h3 className="font-semibold text-2xl rosa">Semi Sr Front-end Developer</h3>
->>>>>>> 2747b3a (Modificaciones experiencia)
                             <p> <strong>CMD - Grupo Clarín</strong>. Frontend developer, Pixel Perfect, PSD to HTML, CSS3, HTML5, Javascript y Jquery.</p>
                         </div>
                     </div>
@@ -85,7 +52,6 @@ export function Experiencia() {
                 <div className="flex flex-row">
                     <div>
                         <div className="pb-2">
-<<<<<<< HEAD
                             <h3 className="font-semibold pt-6 text-gray-800 text-lg">2012-2013</h3>
                             <h3 className="font-semibold text-2xl rosa">Front-end Developer</h3>
                             <p> <strong>Latinlingua</strong> Frontend developer, Pixel Perfect, CSS3, HTML5, Javascript y Jquery.
@@ -100,11 +66,6 @@ export function Experiencia() {
                             <h3 className="font-semibold pt-6 text-gray-800 text-lg">2010 - 2012</h3> 
                             <h3 className="font-semibold text-2xl rosa">Front-end Developer</h3>
                             <p> <strong>Agencia 451</strong>. Frontend developer, CSS animation, Javascript y Jquery.</p>
-=======
-                            <h3 className="font-semibold pt-6 text-gray-800 text-lg">2010 - 2012</h3> 
-                            <h3 className="font-semibold text-2xl rosa">Front-end Developer</h3>
-                            <p> <strong>Agencia 451</strong>. Frontend developer, UI Designer - CSS animation, Javascript y Jquery.</p>
->>>>>>> 2747b3a (Modificaciones experiencia)
                         </div>
                     </div>
                 </div>
@@ -112,13 +73,8 @@ export function Experiencia() {
                 <div className="flex flex-row">
                     <div>
                         <div className="pb-2">
-<<<<<<< HEAD
                             <h3 className="font-semibold pt-6 text-gray-800 text-lg">2009-2010</h3>
                             <h3 className="font-semibold text-2xl rosa">PSD to HTML/CSS</h3>
-=======
-                            <h3 className="font-semibold pt-6 text-gray-800 text-lg">2008-2010</h3>
-                            <h3 className="font-semibold text-2xl rosa">PSD to HTML/CSS/Javascript</h3>
->>>>>>> 2747b3a (Modificaciones experiencia)
                             <p> <strong>Escuela Argentina de Negocios</strong>. Frontend developer, Javascript y Jquery.
                             </p>
                         </div>
